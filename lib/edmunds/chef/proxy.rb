73e2068f97b30992a032b6502a376244d8a64f4e
Dmitriy Vinogradov <dvinogradov@edmunds.com>
2014-05-26 16:14:04 -0700

    Simple proxy test

--- lib/edmunds/chef/proxy.rb
+++ lib/edmunds/chef/proxy.rb
@@ -1,9 +1,39 @@
 require "edmunds/chef/proxy/version"
 
+require "em-proxy"
+
 module Edmunds
   module Chef
     module Proxy
-      # Your code goes here...
+
+      def self.start
+
+        ::Proxy.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |conn|
+          conn.server :srv, :host => "chef11-preprod-app-1.prod-admin1.cloud.edmunds.com", :port => 80
+
+          # modify / process request stream
+          conn.on_data do |data|
+            p [:on_data, data]
+            data
+          end
+
+          # modify / process response stream
+          conn.on_response do |backend, resp|
+            p [:on_response, backend, resp]
+            resp
+          end
+
+          # termination logic
+          conn.on_finish do |backend, name|
+            p [:on_finish, name]
+
+            # terminate connection (in duplex mode, you can terminate when prod is done)
+            unbind if backend == :srv
+          end
+        end
+
+      end
+
     end
   end
 end
