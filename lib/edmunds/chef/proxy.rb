5c2821089acfad69cad446d34a69e821ad56831f
Dmitriy Vinogradov <dvinogradov@edmunds.com>
2014-05-26 17:14:44 -0700

    Adding option parsing

--- lib/edmunds/chef/proxy.rb
+++ lib/edmunds/chef/proxy.rb
@@ -1,6 +1,7 @@
 require "edmunds/chef/proxy/version"
+require "trollop"   # command-line option parser
 
-require "em-proxy"
+require "em-proxy"  # proxy library
 
 module Edmunds
   module Chef
@@ -8,8 +9,24 @@ module Edmunds
 
       def self.start
 
-        ::Proxy.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |conn|
-          conn.server :srv, :host => "chef11-preprod-app-1.prod-admin1.cloud.edmunds.com", :port => 80
+        # Parse options
+        
+        opts = Trollop::options do
+           opt :quiet, "Use minimal output", :short => 'q'
+           opt :verbose, "Output diagnostic info", :short => 'v'
+           opt :chef, "Chef host:port to redirect to", :type => String
+           opt :listen, "Local host:port to listen on", :type => String, :default => "0.0.0.0:80"
+           opt :config, "Configuration file to load", :type => String #, :default => "/etc/edmunds-chef-proxy/config.yml"
+        end
+        Trollop::die :config, "file must exist" unless File.exist?(opts[:config]) if opts[:config]
+        Trollop::die :chef, "required option" if not opts[:chef]
+        Trollop::die :chef, "invalid host:port" if opts[:chef].split(":").count != 2
+        Trollop::die :listen, "invalid host:port" if opts[:listen].split(":").count != 2
+
+        # Start proxy
+        
+        ::Proxy.start(:host => opts[:listen].split(":")[0], :port => opts[:listen].split(":")[1], :debug => opts[:verbose]) do |conn|
+          conn.server :srv, :host => opts[:chef].split(":")[0], :port => Integer(opts[:chef].split(":")[1])
 
           # modify / process request stream
           conn.on_data do |data|
