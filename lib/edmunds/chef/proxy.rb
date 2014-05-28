970704263e130dcdc79ad8f0a2807678cfa43bcd
Dmitriy Vinogradov <dvinogradov@edmunds.com>
2014-05-28 11:15:41 -0700

    Adding settings file

--- lib/edmunds/chef/proxy.rb
+++ lib/edmunds/chef/proxy.rb
@@ -1,4 +1,5 @@
 require "edmunds/chef/proxy/version"
+require "edmunds/chef/proxy/settings"
 require "trollop"   # command-line option parser
 
 require "em-proxy"  # proxy library
@@ -12,16 +13,22 @@ module Edmunds
         # Parse options
         
         opts = Trollop::options do
-           opt :quiet, "Use minimal output", :short => 'q'
-           opt :verbose, "Output diagnostic info", :short => 'v'
-           opt :chef, "Chef host:port to redirect to", :type => String
-           opt :listen, "Local host:port to listen on", :type => String, :default => "0.0.0.0:8080"
-           opt :config, "Configuration file to load", :type => String #, :default => "/etc/edmunds-chef-proxy/config.yml"
+           opt :quiet, "Use minimal output", :short => 'q', :default => false
+           opt :verbose, "Output diagnostic info", :short => 'v', :default => false
+           opt :chef, "Chef host:port to redirect to", :type => String, :short => 'c'
+           opt :listen, "Local host:port to listen on", :type => String, :short => 'l', :default => "0.0.0.0:8080"
+           opt :settings, "Configuration file to load (YAML format)", :type => String, :short => 's' #, :default => "/etc/edmunds-chef-proxy/settings.yml"
         end
-        Trollop::die :config, "file must exist" unless File.exist?(opts[:config]) if opts[:config]
+        Trollop::die :settings, "file must exist" unless File.exist?(opts[:settings]) if opts[:settings]
         Trollop::die :chef, "required option" if not opts[:chef]
         Trollop::die :chef, "invalid host:port" if opts[:chef].split(":").count != 2
         Trollop::die :listen, "invalid host:port" if opts[:listen].split(":").count != 2
+
+        # Load settings
+
+        if opts[:settings]
+          Settings.load!(opts[:settings])
+        end
 
         # Start proxy
         
@@ -30,19 +37,19 @@ module Edmunds
 
           # modify / process request stream
           conn.on_data do |data|
-            p [:on_data, data]
+            p [:on_data, data] if opts[:verbose]
             data
           end
 
           # modify / process response stream
           conn.on_response do |backend, resp|
-            p [:on_response, backend, resp]
+            #p [:on_response, backend, resp] if opts[:verbose]
             resp
           end
 
           # termination logic
           conn.on_finish do |backend, name|
-            p [:on_finish, name]
+            p [:on_finish, name] if opts[:verbose]
 
             # terminate connection (in duplex mode, you can terminate when prod is done)
             unbind if backend == :srv
---
+++ lib/edmunds/chef/proxy/settings.rb
@@ -0,0 +1,30 @@
+require 'yaml'
+require 'deep_symbolize'
+
+module Settings
+  extend self
+
+  @_settings = {}
+  attr_reader :_settings
+
+  def load!(filename)
+    newsets = YAML::load_file(filename)
+    newsets.extend DeepSymbolizable
+    newsets = newsets.deep_symbolize
+    deep_merge!(@_settings, newsets)
+  end
+
+  # Deep merging of hashes
+  def deep_merge!(target, data)
+    merger = proc{|key, v1, v2|
+      Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+    target.merge! data, &merger
+  end
+
+  def method_missing(name, *args, &block)
+    @_settings[name.to_sym] ||
+    fail(NoMethodError, "unknown configuration root #{name}", caller)
+  end
+
+end
+
