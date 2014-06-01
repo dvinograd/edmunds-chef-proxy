9c5cc7d1edc9db155b03def207ea19bcf978d12c
Dmitriy Vinogradov <dvinogradov@edmunds.com>
2014-05-31 17:40:00 -0700

    Rudimentary decision logic

--- lib/edmunds/chef/proxy.rb
+++ lib/edmunds/chef/proxy.rb
@@ -4,6 +4,12 @@ require "edmunds/chef/proxy/version"
 
 require "trollop"   # command-line option parser
 require "em-proxy"  # proxy library
+require "http/parser"
+
+require 'mixlib/authentication/http_authentication_request'
+require 'mixlib/authentication/signatureverification'
+require 'ostruct'
+require 'pp'
 
 module Edmunds
   module Chef
@@ -37,16 +43,41 @@ module Edmunds
         ::Proxy.start(:host => opts[:listen].split(":")[0], :port => opts[:listen].split(":")[1], :debug => opts[:verbose]) do |conn|
           conn.server :srv, :host => opts[:chef].split(":")[0], :port => Integer(opts[:chef].split(":")[1])
 
+          @p = Http::Parser.new
+          @allow = false
+          @p.on_headers_complete = proc do |h|
+            @allow = false
+            # p [:on_headers_complete, h]
+            request = Struct.new(:env, :method, :path)
+            @request = request.new(h, @p.http_method, @p.request_url)
+            p [:request, @request]
+            #@m = ::Mixlib::Authentication::HTTPAuthenticationRequest.new(request)
+            if @p.request_url =~ /role/
+              @allow = true
+            else
+              @allow = false
+            end
+          end
+
+          conn.on_connect do |data,b|
+            puts [:on_connect, data, b].inspect
+          end
+
           # modify / process request stream
           conn.on_data do |data|
             # p [:on_data, data] if opts[:verbose]
-            ::Edmunds::Chef::Processor.process_request(data, settings)
+            @p << data
+            if @allow
+              data
+            else
+              conn.send_data "HTTP/1.1 403 Forbidden\r\n\r\n"
+            end
           end
 
           # modify / process response stream
           conn.on_response do |backend, resp|
             # p [:on_response, backend, resp] if opts[:verbose]
-            resp
+            resp if @allow
           end
 
           # termination logic
--- lib/edmunds/chef/proxy/processor.rb
+++ lib/edmunds/chef/proxy/processor.rb
@@ -1,9 +1,17 @@
+require 'mixlib/authentication/http_authentication_request'
+require 'mixlib/authentication/signatureverification'
+
 module Edmunds
   module Chef
     module Processor
 
       def self.process_request(data, settings)
-        data
+        @m = ::Mixlib::Authentication::HTTPAuthenticationRequest.new(data)
+        if @m.user_id() == "dvinogradov"
+          data
+        else
+          nil
+        end
       end
 
     end
